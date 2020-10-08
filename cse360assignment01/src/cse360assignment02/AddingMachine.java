c297fbae95508afb1a6b4a93f9278b63da80856b
francisekkagit <francisxekka@gmail.com>
2020-10-07 19:01:39 -0700

    Version 1.0

--- cse360assignment01/src/cse360assignment02/AddingMachine.java
+++ cse360assignment01/src/cse360assignment02/AddingMachine.java
@@ -1,27 +1,54 @@
+/**
+ * The Adding Machine adds and subtracts values.
+ * 
+ * @author Francis Ekka
+ * @version 1.0
+ * @since 2020-10-7
+**/
+
 package cse360assignment02;
 
 public class AddingMachine {
-	
-	private int total;
-	  
-	  public AddingMachine () {
-	    total = 0;  // not needed - included for clarity
-	  }
-	  
-	  public int getTotal () {
-	    return 0;
-	  }
-	  
-	  public void add (int value) {
-	  }
+  private int total;
+  
+  public AddingMachine () {
+    total = 0;  // not needed - included for clarity
+  }
+  
+  public int getTotal () {
+    return 0;
+  }
+  
+  /**
+   * This method adds values
+   * @param value
+   * @return nothing
+  **/
+  
+  public void add (int value) {
+  }
+  
+  /**
+   * This method subtracts values
+   * @param value
+   * @return nothing
+  **/
+  public void subtract (int value) {
+  }
+  
+  /**
+   * This method returns the values as strings
+   * @return string of values with operations
+  **/
 
-	  public void subtract (int value) {
-	  }
+  public String toString () {
+    return "";
+  }
 
-	  public String toString () {
-	    return "";
-	  }
-
-	  public void clear() {
-	  }
+  /**
+   * This method clears values and returns total to 0
+   * @return 
+  **/
+  public void clear() {
+  }
 }
